fa74457046f713c3e2ae93801fd7bd02a8d8e051
Giovanna Pezzini <giovannarpezzini@gmail.com>
2025-02-23 09:34:06 +0900

    Save command requests cache deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -5,15 +5,26 @@
 //  Created by giovanna pezzini on 2025/02/23.
 //
 
+import EssentialFeed
 import XCTest
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+
+    func delete(_ items: [FeedItem]) {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class LocalFeedLoader {
+    private let store: FeedStore
+
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+
+    func save(_ items: [FeedItem]) {
+        store.delete(items)
     }
 }
 
@@ -23,5 +34,24 @@ class CacheFeedUseCaseTests: XCTestCase {
         _ = LocalFeedLoader(store: store)
 
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+
+        sut.save(items)
+
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+
+    // MARK: - Helpers
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
     }
 }
